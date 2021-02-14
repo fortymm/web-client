39d6113ba73a10342c8117f32ae74e5b5b0b0e52
Ryan Morris <ryan@ryans-mbp.lan>
2021-02-14 16:42:06 -0600

    feat(matches): add the scaffold for a page to create matches

---
+++ layouts/fullscreen.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="h-screen w-screen">
+    <Nuxt />
+  </div>
+</template>
---
+++ pages/matches/new.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NewMatch />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import NewMatch from '@/components/matches/NewMatch.vue'
+export default Vue.extend({
+  components: { NewMatch },
+  layout: 'fullscreen',
+})
+</script>
