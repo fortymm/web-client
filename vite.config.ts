f03df2c6b5a997fdba87b6a05a6e8dc6203ce036
Ryan Morris <ryan.morris@centro.net>
2024-06-20 06:08:13 -0600

    ci: :construction_worker: run the end to end tests on each push

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 5173,
+  },
   test: {
     environment: "happy-dom",
     setupFiles: "./setupTests.ts",
